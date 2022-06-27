8deff9ef5943e9f49e5336a6e9d28a7eff7d1c83
Tat8y <98891553+Tat8y@users.noreply.github.com>
2022-06-28 00:50:03 +0530

    Add Payment Button Function

--- lib/pages/checkout/payment_confirmation/paymnet_confrimation.dart
+++ lib/pages/checkout/payment_confirmation/paymnet_confrimation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:travel_smartapp/domain/payment/payment_exception.dart';
+import 'package:travel_smartapp/domain/payment/payment_service.dart';
 import 'package:travel_smartapp/pages/booking/code/booking_code.dart';
 
 class PaymentConfirmation extends StatelessWidget {
@@ -10,10 +12,21 @@ class PaymentConfirmation extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
             child: const Text("Confirm Payment"),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (_) {
-                return const BookingCode();
-              }));
+            onPressed: () async {
+              try {
+                await PaymentService.instance
+                    .makePayment(amount: '250')
+                    .then((value) {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return const BookingCode();
+                  }));
+                });
+              } on PaymentCancelException {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Payment Canceled"),
+                  duration: Duration(seconds: 1),
+                ));
+              } catch (e) {}
             }),
       ),
     );
