ff7423f5e9112690ab5c9c8adaa61c640b641816
Tat8y <98891553+Tat8y@users.noreply.github.com>
2022-06-29 11:30:38 +0530

    Splash Screen Fixed

--- lib/pages/checkout/payment_status/payment_cancel.dart
+++ lib/pages/checkout/payment_status/payment_cancel.dart
@@ -4,12 +4,13 @@ import 'package:travel_smartapp/utils/dialog/generic_dialog.dart';
 Future<void> openPaymentCancelled(BuildContext context) async {
   return await showGenericDialog<void>(
     context,
-    color: Colors.orange,
-    icon: Icons.bolt_rounded,
+    color: Colors.red,
+    icon: Icons.cancel_rounded,
     header: "Payment Cancelled",
     content: "Payment Error Body",
     callBack: () => {
       GenericDialogButton(text: "CLOSE", primary: true): null,
+      GenericDialogButton(text: "OK"): null,
     },
   );
 }
